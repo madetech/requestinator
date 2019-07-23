ba2c05fc7f58ab357845b54721295f1e46ac360d
Maysa <maysa@madetech.com>
2019-07-23 13:31:21 +0100

    update test name

--- spec/unit/use_case/view_requests_summary_spec.rb
+++ spec/unit/use_case/view_requests_summary_spec.rb
@@ -3,7 +3,7 @@ describe UseCase::ViewRequestsSummary do
   let(:view_requests_summary) { described_class.new(google_spreadsheet_gateway: google_spreadsheet_gateway) }
   let(:response) { view_requests_summary.execute }
 
-  it 'can show client as the first key for the data' do
+  it 'can return the correct summary data for requests' do
     expect(response).to eq({active:1, in_progress:0})
   end
 end
